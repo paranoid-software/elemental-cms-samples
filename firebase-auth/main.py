42a044bb421a4e08aaebedc46062cdf6564dd3b7
Pablo Pinargote <pablo.pinargote@outlook.com>
2022-05-01 16:10:28 -0500

    Config file setup support added

--- firebase-auth/main.py
+++ firebase-auth/main.py
@@ -8,9 +8,9 @@ from flask import Flask
 
 www = Flask(__name__, template_folder='templates', static_folder='static')
 
-CONFIG_FILE_NAME = os.environ.get('CONFIG_FILE_NAME', 'settings/debug.json')
+CONFIG_FILE_PATH = os.environ.get('CONFIG_FILE_PATH', 'settings/debug.json')
 
-with open(CONFIG_FILE_NAME) as config_file:
+with open(CONFIG_FILE_PATH) as config_file:
     settings = json.load(config_file)
     cms_core_context = FlaskContext(settings['cmsCoreContext'])
     cms_db_context = MongoDbContext(settings['cmsDbContext'])
